0fef8c13957560ab1887650101ddc21a9b6b2afc
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2024-11-22 12:17:24 +0530

    modify

--- api/config/database.js
+++ api/config/database.js
@@ -2,10 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASSWORD,
-  "bookstore","postgres","root",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
